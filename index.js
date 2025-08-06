ea85692456d24aacced8abc5de3352320ebdec44
forjoe <iosif.dahdal@gmail.com>
2025-08-06 19:44:42 +0300

    A lot of changes has been made

--- index.js
+++ index.js
@@ -23,13 +23,12 @@ document.querySelectorAll('.project-image').forEach(box => {
 
 closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
-  iframe.src = ""; // clear iframe
+  iframe.src = "";
 });
 
-// Close modal when clicking outside the modal content
 modal.addEventListener('click', (e) => {
   if (e.target === modal) {
     modal.style.display = 'none';
-    iframe.src = ''; // clear iframe
+    iframe.src = '';
   }
 });
