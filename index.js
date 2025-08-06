26bfb5d06ce1689683f4938c4e401ba8bbc18f79
forjoe <iosif.dahdal@gmail.com>
2025-08-06 18:14:37 +0300

    Projects page has been updated

---
+++ index.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.project-image').forEach(el => {
+    const img = el.getAttribute('data-img');
+    if (img) {
+      el.style.backgroundImage = `url('${img}')`;
+    }
+  });
+});
+
+const modal = document.getElementById("preview-modal");
+const iframe = document.getElementById("modal-iframe");
+const closeBtn = document.getElementById("close-modal");
+
+document.querySelectorAll('.project-image').forEach(box => {
+  box.addEventListener('click', () => {
+    const url = box.getAttribute('data-url');
+    if (!url) return;
+
+    iframe.src = url;
+    modal.style.display = 'flex';
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+  iframe.src = ""; // clear iframe
+});
+
+// Close modal when clicking outside the modal content
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+    iframe.src = ''; // clear iframe
+  }
+});
